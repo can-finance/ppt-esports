0aa25ad1b2decd1794fb35c384d6fce0af05866f
can-finance <tripleq@gmail.com>
2026-03-31 09:50:46 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ppt-esports/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
